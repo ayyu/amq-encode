64d93b557dde331270336b874cc69e710dee6623
ayyu <ayyu@uwaterloo.ca>
2021-08-10 02:19:26 -0400

    updated audio settings to reflect new upload FAQ

--- amq_settings.sh
+++ amq_settings.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-target_max=-1.0
-target_mean=-18.0
+target_max=-0.5
+target_mean=-18.5
 meta_settings="-map_metadata -1 -map_chapters -1 -sn"
 audio_settings="-b:a 320k -ac 2"
 opus_settings="-c:a libopus $audio_settings"
