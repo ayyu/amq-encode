828f007f5f9e9da4fcd70b6b846ba1ac22e485d0
ayyu <ayyu@uwaterloo.ca>
2021-12-31 13:11:20 -0500

    fixed sample encode

--- sample_encode.py
+++ sample_encode.py
@@ -5,7 +5,5 @@ clean_audio = 'clean.wav'
 source_dir = './source/'
 clean_dir = './clean/'
 
-# encode file to all resolutions except 360
-# put the outputs in a folder called source
 amqencode.encode.encode_all(input_file, source_dir, norm=True, ss="1:39", to="5:20")
 amqencode.encode.mux_folder(source_dir, clean_audio, clean_dir, norm=True)
